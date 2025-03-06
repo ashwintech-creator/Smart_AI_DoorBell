f5542b759d82167a799cffdb112e69b9f8bd8aa2
Ashwinvarma T <117467065+ashwintech-creator@users.noreply.github.com>
2025-03-06 23:41:42 +0530

    Add files via upload

---
+++ facerecgonition.py
@@ -0,0 +1,39 @@
+import cv2
+import face_recognition
+
+# Load known face and encode it
+known_image = face_recognition.load_image_file("/Users/shabarinathankrv/Documents/VS/python/facerecgonition/6379384048.jpg")
+  # Replace with your image
+known_encoding = face_recognition.face_encodings(known_image)[0]
+known_faces = [known_encoding]
+known_names = ["shabari"]
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        matches = face_recognition.compare_faces(known_faces, face_encoding)
+        name = "Unknown"
+
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_names[first_match_index]
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    cv2.imshow('Face Recognition', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
